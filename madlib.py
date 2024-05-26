bf8dca72753e1c01cefec9a92bd34254b5383dd7
karthik8147 <122341137+karthik8147@users.noreply.github.com>
2024-05-26 22:44:43 +0530

    first commit

---
+++ madlib.py
@@ -0,0 +1,7 @@
+adj= input("Adjectve: ")
+past= input("cllas: ")
+varb1= input("Varb: ")
+
+madlib = f"My Nmae is {adj} i love my Dad Most but unfortunatly we lost him when i was in {past} \
+and he was my {varb1} after he pass out my mom and my sister is {varb1}!  "
+print (madlib)
